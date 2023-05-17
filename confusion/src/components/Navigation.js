1c1271450a3ba7f1029aed64df4de8e32a0debb4
Nam An <nguynphmnaman@gmail.com>
2023-05-17 09:57:48 +0700

    Presentational and Container Components

---
+++ confusion/src/components/Navigation.js
@@ -0,0 +1,13 @@
+export default function Navigation() {
+    return (
+      <nav className='back-ground'>
+    <ul>
+        <a className='active' href='#home'>Home</a>
+        <a className='active' href='#news'>News</a>
+        <a className='active' href='#about'>About</a>
+        <a className='active' href='#contact'>Contact</a>
+    </ul>
+  </nav>
+    )
+  }
+  
