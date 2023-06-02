d4511b8675df603a4f15706c98a7e752189cb1fd
Nam An <nguynphmnaman@gmail.com>
2023-06-02 16:37:24 +0700

    Functional Components

--- confusion/src/components/Navigation.js
+++ confusion/src/components/Navigation.js
@@ -1,15 +1,15 @@
-import React,{ Component} from 'react';
-export default class Navigation extends Component {
-    render(){
-        return (
+import React from 'react';
+export default function Navigation() {
+    return (
+        <div class='box'>
             <div className='task'>
                 <nav>
-                <a className='active' href='#home'>Home</a>
-                <a className='active' href='#news'>News</a>
-                <a className='active' href='#about'>About</a>
-                <a className='active' href='#contact'>Contact</a>
+                    <a className='active' href='#home'>Home</a>
+                    <a className='active' href='#news'>News</a>
+                    <a className='active' href='#about'>About</a>
+                    <a className='active' href='#contact'>Contact</a>
                 </nav>
             </div>
-        )
-    }
+        </div>
+    )
 }
